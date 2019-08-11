fb08e48a29c1bbc4a5419274f67c179203b770b2
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-08-11 12:00:26 +0300

    Updates

---
+++ precache-manifest.e5846792f2663db082c3f9e708911a3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6738fe01fa2b2d7f6c901deb2c5ba59a",
+    "url": "/react_phone-my-phone-catalog/index.html"
+  },
+  {
+    "revision": "3c4811f9bd7dc8e03b70",
+    "url": "/react_phone-my-phone-catalog/static/css/main.1a469c3a.chunk.css"
+  },
+  {
+    "revision": "1b70710b463b94a659d4",
+    "url": "/react_phone-my-phone-catalog/static/js/2.4376b73a.chunk.js"
+  },
+  {
+    "revision": "3c4811f9bd7dc8e03b70",
+    "url": "/react_phone-my-phone-catalog/static/js/main.182deb6e.chunk.js"
+  },
+  {
+    "revision": "0c6de5c9ce6aac81e792",
+    "url": "/react_phone-my-phone-catalog/static/js/runtime~main.f82f3c60.js"
+  }
+]);
